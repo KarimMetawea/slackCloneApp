12ec2f86e2680ce57b9e4b00038302cdb2f8fa6b
karim metawea <karimmetawea@karims-MacBook-Air.local>
2019-02-20 20:12:06 +0200

    setup project

---
+++ slackCloneApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slackCloneApp
+//
+//  Created by karim metawea on 2/20/19.
+//  Copyright © 2019 karim metawea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
